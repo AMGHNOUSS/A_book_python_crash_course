4face4dc48b3ee2c8a2b0cd152c4661b695c7423
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-07-30 23:26:03 +0100

    Create File bicycles.py

---
+++ Chapter_03/bicycles.py
@@ -0,0 +1,5 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+print(bicycles[0].title())
+print(bicycles[-1])
+print(f"My first bicycles was : {bicycles[2].title()}")
