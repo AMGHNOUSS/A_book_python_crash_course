fc9bbd4657b90c6d186f0ecab0b7872bd5223d16
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-07-31 15:46:30 +0100

    Update The code of File cars.py

--- Chapter_03/cars.py
+++ Chapter_03/cars.py
@@ -1,2 +1,17 @@
-cars = ['BMW', 'Honda', 'Mercedes', 'volkswagen']
+cars = ['BMW', 'Audi', 'Toyota', 'Honda', 'Mercedes', 'Volkswagen']
 print(f"I would like to own a {cars[3].title()} car.")
+print("+=================+")
+cars.sort() # Sorting By Alphabetical, and it's change itself
+print(cars)
+cars.sort(reverse = True) # Sorting By  Reverse Alphabetical
+print(cars)
+print("+=================+")
+print(sorted(cars))
+print("+=================+")
+# Printing a List in Reverse Order
+cars = ['BMW', 'Audi', 'Toyota', 'Honda', 'Mercedes', 'Volkswagen']
+print(cars)
+cars.reverse()
+print(cars)
+print("+======================+")
+print(f"Lenght of list cars: {len(cars)}")
