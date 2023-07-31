1ba9431da4e40061c2d61f139ec99366335fd411
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-07-31 14:51:41 +0100

    Create File changing_guest_list.py

---
+++ Chapter_03/changing_guest_list.py
@@ -0,0 +1,9 @@
+names = ['Muhamed', 'Hachim', 'Mansour', 'Ali']
+
+# The  guests can’t make the dinner : Mansour.
+# So we gonna replacing Mansour Shamsi
+names[2] = 'Shamsi'
+print(f"{names[0].title()}, you are invite to dinner")
+print(f"{names[1].title()}, you are invite to dinner")
+print(f"{names[2].title()}, you are invite to dinner")
+print(f"{names[3].title()}, you are invite to dinner")
