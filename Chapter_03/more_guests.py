ac445c74e7b550e359f0ef55e1332bc08ec164ad
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-07-31 14:58:18 +0100

    Create File more_guests.py

---
+++ Chapter_03/more_guests.py
@@ -0,0 +1,11 @@
+names = ['Muhamed', 'Hachim', 'Mansour', 'Ali']
+names.insert(0, 'Hamza')
+names.insert(3, 'Othmane')
+names.append('Adnane')
+print(f"{names[0].title()}, you are invite to dinner")
+print(f"{names[1].title()}, you are invite to dinner")
+print(f"{names[2].title()}, you are invite to dinner")
+print(f"{names[3].title()}, you are invite to dinner")
+print(f"{names[4].title()}, you are invite to dinner")
+print(f"{names[5].title()}, you are invite to dinner")
+print(f"{names[6].title()}, you are invite to dinner")
