198ecd13829be98452ae343a6f9319aebc029b56
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-07-31 14:47:29 +0100

    Update The of File guest_name.py

--- Chapter_03/guest_list.py
+++ Chapter_03/guest_list.py
@@ -1,3 +1,5 @@
 names = ['Muhamed', 'Hachim', 'Mansour', 'Ali']
 print(f"{names[0].title()}, you are invite to dinner")
+print(f"{names[1].title()}, you are invite to dinner")
+print(f"{names[2].title()}, you are invite to dinner")
 print(f"{names[3].title()}, you are invite to dinner")
