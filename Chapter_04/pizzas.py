c095a17f31e0e58e189f21dabc6de718435715e1
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-08-01 20:02:29 +0100

    Create File pizzas.py

---
+++ Chapter_04/pizzas.py
@@ -0,0 +1,7 @@
+pizzas = ['greek', 'sicilian', 'roman', 'neapolitan']
+for pizza in pizzas:
+    print(pizza.title())
+print("+===================+")
+for pizza in pizzas:
+    print(f"I like {pizza.title()} pizza.")
+print("I really love pizza!")
