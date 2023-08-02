dc2ebe3d36ca0b1c04d7be35341245a505ec8325
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-08-02 16:09:06 +0100

    Create File buffet.py

---
+++ Chapter_04/buffet.py
@@ -0,0 +1,9 @@
+tuple_buffet = ('pizza', 'panini', 'tacos', 'sandwich', 'hamberger')
+for food in tuple_buffet:
+    print(food)
+
+# tuple_buffet[2] = 'tajin'
+
+tuple_buffet = ('pizza', 'hot dog', 'tacos', 'sandwich', 'Cheeseburger')
+for food in tuple_buffet:
+    print(food)
