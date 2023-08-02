7496653b5d887fdc51eae68b984f4525a5a25dd8
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-08-02 14:02:37 +0100

    CreateFile animals.py

---
+++ Chapter_04/animals.py
@@ -0,0 +1,7 @@
+animals = ['dog', 'cat', 'rabbit', 'turtle']
+for animal in animals:
+    print(animal)
+print("+=============+")
+for animal in animals:
+    print(f"A {animal.title()} would make a great pet.")
+print("Any of these animals would make a great pet!")
