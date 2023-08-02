b63be2beeaa9d7d7249137db702d2a52c8dc3b9e
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-08-02 15:47:54 +0100

    Create File slices.py

---
+++ Chapter_04/slices.py
@@ -0,0 +1,10 @@
+animals = ['dog', 'cat', 'rabbit', 'turtle', 'goat']
+
+print("The first three items in the list are:")
+print(animals[:3])
+
+print("Three items from the middle of the list are:")
+print(animals[1:4])
+
+print("Three items from the last of the list are:")
+print(animals[-3:])
