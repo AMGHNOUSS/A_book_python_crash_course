6a9a201318a065a3120397110ef417cd8c008cb9
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-08-05 15:18:13 +0100

    Create File favorite_languages.py

---
+++ Chapter_06/favorite_languages.py
@@ -0,0 +1,8 @@
+favorite_languages = {
+ 'jen': 'python',
+ 'sarah': 'c',
+ 'edward': 'rust',
+ 'phil': 'python',
+ }
+language = favorite_languages['sarah'].title()
+print(f"Sarah's favorite language is {language}.")
