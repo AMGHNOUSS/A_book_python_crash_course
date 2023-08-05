2b7ab802ae249ee0d3b982f2840ee022fe20b06e
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-08-05 22:59:05 +0100

    Create File favorite_numbers.py

---
+++ Chapter_06/favorite_numbers.py
@@ -0,0 +1,6 @@
+favorite_numbers = {'Redouane': 6, 'Hamza': 7, 'Brahim': 2, 'Said': 4, 'Yassine': 12}
+print(f"Redouane's favorite number is {favorite_numbers['Redouane']}.")
+print(f"Hamza's favorite number is {favorite_numbers['Hamza']}.")
+print(f"Brahim's favorite number is {favorite_numbers['Brahim']}.")
+print(f"Said's favorite number is {favorite_numbers['Said']}.")
+print(f"Yassine's favorite number is {favorite_numbers['Yassine']}.")
