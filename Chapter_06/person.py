72d29bf8649164439530f1530dd8e53e54a32aaa
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-08-05 23:00:42 +0100

    Create File person.py

---
+++ Chapter_06/person.py
@@ -0,0 +1,5 @@
+person = {'first_name': 'Hamza', 'last_name':'Al kadi', 'age': 22, 'city': 'Laayoune'}
+print(f"Fisrt name: {person['first_name']}")
+print(f"Last name: {person['last_name']}")
+print(f"Age: {person['age']}")
+print(f"City: {person['city']}")
