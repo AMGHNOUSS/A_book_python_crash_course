82ed3b9f17321689cbd2a8ac10dc6eaf0a5eff66
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-08-06 01:34:58 +0100

    Update File favorite_languages.py

--- Chapter_06/favorite_languages.py
+++ Chapter_06/favorite_languages.py
@@ -6,3 +6,7 @@ favorite_languages = {
  }
 language = favorite_languages['sarah'].title()
 print(f"Sarah's favorite language is {language}.")
+
+# Looping Through All the Keys in a Dictionary
+for name, language in favorite_languages.items():
+ print(f"{name.title()}'s favorite language is {language.title()}.")
