9b94c8324c9ba99cb7a501a1d5b5cc953228476e
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-08-06 14:05:57 +0100

    Create File glossary_2.py

---
+++ Chapter_06/glossary_2.py
@@ -0,0 +1,7 @@
+programming = {'Variable': 'is value that can change depending on conditions or on information passed to the program.',
+               'If statement': 'a selection statement that allows more than one possible flow of control.',
+               'Loop': 'a sequence of instruction s that is continually repeated until a certain condition is reached.',
+               'List': 'a sequence of several variables, grouped together under a single name',
+               'Tuple': 'are used to store multiple items in a single variable.'}
+for key, value in programming.items():
+    print(f"\n{key}\n{value}")
