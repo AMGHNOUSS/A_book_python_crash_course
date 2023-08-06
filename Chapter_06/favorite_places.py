a5b9a757e251a46c87ac9645f62df9fa7a9d3cbf
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-08-06 18:12:07 +0100

    Create File favorite_places.py

---
+++ Chapter_06/favorite_places.py
@@ -0,0 +1,3 @@
+favorite_places = {'redouane': 'Laayoune', 'ali': 'london', 'abdellah': 'agadir'}
+for key, value in favorite_places.items():
+    print(f"{key.title()}'s favorite place is {value.title()}.")
