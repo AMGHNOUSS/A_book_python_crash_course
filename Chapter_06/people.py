f9c091153cf6b82e8c574e0388abf02cfb06fe34
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-08-06 17:53:55 +0100

    Create File people.py

---
+++ Chapter_06/people.py
@@ -0,0 +1,7 @@
+person_1 = {'first_name': 'Hamza', 'last_name':'Al kadi', 'age': 22, 'city': 'Laayoune'}
+person_2 = {'first_name': 'John', 'last_name':'destin', 'age': 24, 'city': 'new yotk'}
+
+person_list = [person_1, person_2]
+
+for value in person_list:
+    print(f"{value['first_name'].title()} {value['last_name'].upper()}, his age is {value['age']} and he lives in {value['city'].title()}")
