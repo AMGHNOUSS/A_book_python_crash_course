45ff248a88819a440ab8fa11e2af2bab1aecd1f9
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-09-11 01:03:55 +0100

    Update the class ship.py

--- Chapter_12/ship.py
+++ Chapter_12/ship.py
@@ -5,13 +5,13 @@ class Ship:
 
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
-        self.screen = ai_game
-        self.screen_rect = ai_game.screen.get_rect
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
         # Load the ship image and get its rect.
-        self.image = pygame.image.load('/images/ship.bmp')
+        self.image = pygame.image.load("Chapter_12/images/ship.bmp")
         self.rect = self.image.get_rect()
         # Start each new ship at the bottom center of the screen.
-        self.rect.midbottom = self.screen_rect.milbottom
+        self.rect.midbottom = self.screen_rect.midbottom
     
     def blitme(self):
         """Draw the ship at its current location."""
