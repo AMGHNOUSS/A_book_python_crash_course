86327485ef3c2b038ab7143baa26fbe702f3796c
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-10-21 14:50:17 +0100

    Create File city.py

---
+++ Chapter_11/city.py
@@ -0,0 +1,5 @@
+from city_functions import get_formatted_city
+
+print(get_formatted_city("Ramallah", "Palestine"))
+print(get_formatted_city("Casablanca", "Morocco"))
+print(get_formatted_city("Santiago", "Chile"))
