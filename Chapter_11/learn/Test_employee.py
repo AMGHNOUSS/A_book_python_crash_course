47d2b91b6c4c0924cc5a53645550a2de3892b403
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-10-25 18:50:26 +0100

    FUnction test fullname raise

--- Chapter_11/learn/Test_employee.py
+++ Chapter_11/learn/Test_employee.py
@@ -19,3 +19,30 @@ class TestEmployee(unittest.TestCase):
 
         self.assertEqual(em1.email, "David.Bourne@email.com")
         self.assertEqual(em2.email, "Lara.Smith@email.com")
+    
+
+    def test_fullname(self):
+        """Testing Full name"""
+        
+        em1 = Employee("John", "Bourne", 4000)
+        em2 = Employee("Jane", "Smith", 5000)
+
+        self.assertEqual(em1.fullname, "John Bourne")
+        self.assertEqual(em2.fullname, "Jane Smith")
+
+        em1.first = 'David'
+        em2.first = 'Lara'
+
+        self.assertEqual(em1.fullname, "David Bourne")
+        self.assertEqual(em2.fullname, "Lara Smith")
+    
+    def test_raise(self):
+        """Testing raise"""
+        em1 = Employee("John", "Bourne", 4000)
+        em2 = Employee("Jane", "Smith", 5000)
+
+        em1.apply_raise()
+        em2.apply_raise()
+
+        self.assertEqual(em1.pay, 4200)
+        self.assertEqual(em2.pay, 5250)
